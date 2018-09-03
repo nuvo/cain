dc61a04daa221616f8210157945867aee4241a13
Maor Friedman <maor.friedman@nuvo-group.com>
2018-09-03 10:53:13 +0300

    add code example

---
+++ examples/code/example.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/maorfr/cain/pkg/cain"
+)
+
+func main() {
+
+	namespace := "default"
+	selector := "release=cassandra"
+	container := "cassandra"
+	keyspace := "keyspace"
+	parallel := 0 // all at once
+
+	// Backup
+	dst := "s3://bucket/cassandra"
+	tag, err := cain.Backup(namespace, selector, container, keyspace, dst, parallel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Restore
+	src := "s3://bucket/cassandra/namespace/cluster-name"
+	if err := cain.Restore(src, keyspace, tag, namespace, selector, container, parallel); err != nil {
+		log.Fatal(err)
+	}
+
+	// Schema
+	schema, sum, err := cain.Schema(namespace, selector, container, keyspace)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("sum: %s", sum)
+	fmt.Println(schema)
+}
